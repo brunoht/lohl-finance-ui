d83130e515f70070af60d2be4f5ca5a870aecaae
Bruno Henrique Trindade <bruno.trindade@grancursosonline.com.br>
2024-01-13 23:59:08 -0300

    feat: add store with pinia

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router'
+import VueCookies from 'vue-cookies'
 import './style.css'
 import App from './App.vue'
-import router from './router/index.js'
-import VueCookies from 'vue-cookies'
 
-createApp(App)
-    .use(VueCookies, {expires: '1d'})
-    .use(router)
-    .mount('#app')
+const pinia = createPinia()
+
+const app = createApp(App)
+
+app.use(VueCookies, {expires: '1d'})
+app.use(pinia)
+app.use(router)
+app.mount('#app')
