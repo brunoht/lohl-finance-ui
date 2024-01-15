81ed1bff0bb51b9eb6bf62a149dc63f7e6abfabb
Bruno Henrique Trindade <bruno.trindade@grancursosonline.com.br>
2024-01-15 03:19:55 -0300

    feat: added .env

--- src/services/echo.js
+++ src/services/echo.js
@@ -3,21 +3,14 @@ import Pusher from 'pusher-js'
 
 window.Pusher = Pusher
 
-// const echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: '2c59acd064e79eed07f3',
-//     cluster: 'us2',
-//     forceTLS: true
-// });
-
 const echo = new Echo({
     broadcaster: 'pusher',
-    key: '2c59acd064e79eed07f3',
-    wsHost: 'localhost',
-    wsPort: '6001',
-    cluster: 'us2',
-    forceTLS: false,
-    disableStats: true,
+    key: import.meta.env.VITE_ECHO_KEY,
+    wsHost: import.meta.env.VITE_ECHO_WS_HOST,
+    wsPort: import.meta.env.VITE_ECHO_WS_PORT,
+    cluster: import.meta.env.VITE_ECHO_CLUSTER,
+    forceTLS: import.meta.env.VITE_ECHO_TLS === 'true',
+    disableStats: import.meta.env.VITE_ECHO_DISABLE_STATS === 'true',
 });
 
 export default echo
